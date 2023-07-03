8c1da647d953264eaac33b05fa083269a40c1416
Leo Smith <p4p1@vivaldi.net>
2023-07-03 14:20:04 +0200

    Working version

--- burp-sitemap-to-notion.py
+++ burp-sitemap-to-notion.py
@@ -1,20 +1,17 @@
 from burp import IBurpExtender, IHttpListener, ITab
-import csv
 from javax import swing
 from javax.swing import JFileChooser
 from javax.swing import JTable
 from javax.swing.table import DefaultTableModel
 from javax.swing.filechooser import FileNameExtensionFilter
 from javax.swing import JOptionPane
+from java.awt import Dimension
 from java.awt import Color
 from java.awt import Font
 import java.lang as lang
 
 import json
 import urllib2
-
-BASE_URL = "https://api.notion.com/%s"
-TOKEN="secret_V2PSktPqOfODZHDJemXZqGlVee3ozH6EFgCdEC1Ojo1"
 
 class NotionWrapper():
     def __init__(self, token, base_url="https://api.notion.com/%s"):
@@ -46,7 +43,6 @@ class NotionWrapper():
             }
         }
         json_data = self.do_api_request('v1/search', body)
-        self.first_id = json_data["results"][0]["id"] 
         return json_data["results"]
 
     def get_database_entries(self, db_id):
@@ -57,7 +53,7 @@ class NotionWrapper():
         json_data = self.do_api_request('v1/databases/%s/query' % db_id, body)
         return json_data["results"]
 
-    def create_page_from_template(self, parent_id, title):
+    def create_root_page_from_template(self, parent_id, title):
         body = {
             "parent": {
                 "database_id": parent_id
@@ -85,41 +81,44 @@ class NotionWrapper():
                     "heading_2": {
                         "rich_text": [{ "type": "text", "text": { "content": "Endpoints:" } }]
                     }
-                },
-                {
-                    "object": "block",
-                    "type": "heading_2",
-                    "heading_2": {
-                        "rich_text": [{ "type": "text", "text": { "content": "Scans:" } }]
-                    }
-                },
-                {
-                    "object": "block",
-                    "type": "code",
-                    "code": {
-                        "caption": [],
-                        "rich_text": [{ "type": "text", "text": { "content": "...." } }],
-                        "language": "bash"
-                    }
-                },
-                {
-                    "object": "block",
-                    "type": "heading_2",
-                    "heading_2": {
-                        "rich_text": [{ "type": "text", "text": { "content": "Screenshots:" } }]
-                    }
                 }
             ]
         }
         json_data = self.do_api_request('v1/pages', body)
-        print(json_data)
         return json_data["id"]
 
-    def create_database_from_template(self, parent_id):
+    def create_child_page_from_template(self, parent_id, title, method):
+        body = {
+            "parent": {
+                "database_id": parent_id
+            },
+            "properties": {
+                "Endpoint": {
+                    "title": [
+                        {
+                            "text": {
+                                "content": title
+                            }
+                        }
+                    ]
+                },
+                "Method": {
+                    "select": {
+                        "name": method
+                    }
+                }
+            },
+            "children": [
+            ]
+        }
+        json_data = self.do_api_request('v1/pages', body)
+        return json_data["id"]
+
+    def create_root_database_from_template(self, parent_id, isinline=False):
         body = {
             "parent": {
                 "type": "page_id",
-                "page_id": self.first_id
+                "page_id": parent_id
             },
             "title": [
                 {
@@ -129,6 +128,7 @@ class NotionWrapper():
                     }
                 }
             ],
+            "is_inline": isinline,
             "properties": {
                 "Name": {
                     "title": {}
@@ -160,27 +160,87 @@ class NotionWrapper():
         json_data = self.do_api_request('v1/databases', body)
         return json_data["id"]
 
+    def create_inline_database_from_template(self, parent_id, isinline=True):
+        body = {
+            "parent": {
+                "type": "page_id",
+                "page_id": parent_id
+            },
+            "title": [
+                {
+                    "type": "text",
+                    "text": {
+                        "content": "Requests"
+                    }
+                }
+            ],
+            "is_inline": isinline,
+            "properties": {
+                "Method": {
+                    "select": {
+                        "options": [
+                            {
+                                "name": "PATCH",
+                                "color": "gray"
+                            },
+                            {
+                                "name": "POST",
+                                "color": "orange"
+                            },
+                            {
+                                "name": "GET",
+                                "color": "green"
+                            },
+                            {
+                                "name": "DELETE",
+                                "color": "red"
+                            },
+                            {
+                                "name": "PUT",
+                                "color": "blue"
+                            }
+                        ]
+                    }
+                },
+                "Endpoint": {
+                    "title": {}
+                }
+            }
+        }
+        json_data = self.do_api_request('v1/databases', body)
+        return json_data["id"]
+
 class BurpExtender(IBurpExtender, IHttpListener, ITab):
     def registerExtenderCallbacks(self, callbacks):
-        self.nw = NotionWrapper(token=TOKEN)
         self._callbacks = callbacks
+        self.nw = None
+        self.root_id = None
+        self.notion_id_list = []
+        self.notion_title_list = []
         self._helpers = callbacks.getHelpers()
         self.colName = [ 'Method', 'status', 'path' ]
         self.bigest_length = 0
-        self.csv_data = []
+        self.req_data = []
         self.drawUI()
         callbacks.registerHttpListener(self)
         callbacks.addSuiteTab(self)
         callbacks.setExtensionName("SiteMap To Notion")
-        self.nw.get_pages()
-        idnum = self.nw.create_database_from_template("replace_with_id")
-        self.nw.create_page_from_template(idnum, '/')
-        print(self.nw.get_database_entries(idnum))
+
 
     def drawUI(self):
         self.tab = swing.JPanel()
         # This part is just for the export button
-        self.uiExportRun = swing.JButton('Export',actionPerformed=self.exportAndSaveCsv)
+        self.uiTitleLabel= swing.JLabel('Please set notion token and select the correct shared page before running:')
+        self.uiTitleLabel.setFont(Font('Tahoma', Font.BOLD, 14))
+        self.uiTokenField = swing.JTextField('insert Token')
+        self.uiTokenField.setPreferredSize(Dimension(200, 75))
+        self.uiSaveToken = swing.JButton('Save Token',actionPerformed=self.wraperToExportNotion)
+        # pick root page
+        self.uiRootPageLabel = swing.JLabel('Select Page:')
+        self.uiRootPageLabel.setFont(Font('Tahoma', Font.BOLD, 14))
+        self.uiRootPagePane = swing.JScrollPane()
+        # button to run export
+        self.uiExportRun = swing.JButton('Export to Notion',actionPerformed=self.exportAndSaveToNotion)
         # This part will be for the log panel
         self.uiLogLabel = swing.JLabel('Log:')
         self.uiLogLabel.setFont(Font('Tahoma', Font.BOLD, 14))
@@ -194,7 +254,14 @@ class BurpExtender(IBurpExtender, IHttpListener, ITab):
                                   .addGroup(layout.createSequentialGroup()
                                             .addGap(10,10,10)
                                             .addGroup(layout.createParallelGroup(swing.GroupLayout.Alignment.LEADING)
-                                                      .addComponent(self.uiExportRun)
+                                                        .addComponent(self.uiTitleLabel)
+                                                        .addComponent(self.uiTokenField)
+                                                        .addComponent(self.uiSaveToken)
+                                                        .addGap(15,15,15)
+                                                        .addComponent(self.uiRootPageLabel)
+                                                        .addComponent(self.uiRootPagePane)
+                                                        .addGap(15,15,15)
+                                                        .addComponent(self.uiExportRun)
                                                         .addGap(15,15,15)
                                                         .addComponent(self.uiLogLabel)
                                                         .addComponent(self.uiLogPane)
@@ -205,6 +272,16 @@ class BurpExtender(IBurpExtender, IHttpListener, ITab):
         # For some reason you need horizontal and vertical group for it to work :/
         layout.setVerticalGroup(layout.createParallelGroup(swing.GroupLayout.Alignment.LEADING)
                                   .addGroup(layout.createSequentialGroup()
+                                            .addGap(15,15,15)
+                                            .addComponent(self.uiTitleLabel)
+                                            .addGap(15,15,15)
+                                            .addComponent(self.uiTokenField)
+                                            .addGap(5,5,5)
+                                            .addComponent(self.uiSaveToken)
+                                            .addGap(15,15,15)
+                                            .addComponent(self.uiRootPageLabel)
+                                            .addGap(5,5,5)
+                                            .addComponent(self.uiRootPagePane)
                                             .addGap(15,15,15)
                                             .addComponent(self.uiExportRun)
                                             .addGap(20,20,20)
@@ -221,65 +298,68 @@ class BurpExtender(IBurpExtender, IHttpListener, ITab):
     def getUiComponent(self):
         return self.tab
 
-    def exportAndSaveCsv(self, e):
-        f, ok = self.openFile('csv', 'CSV Files', 'wb')
-        if ok:
-            self.writer = csv.writer(f)
-            self.writer.writerow(self.colName)
-            for line in self.csv_data:
-                self.writer.writerow(line)
-            f.close()
+    def menu_click(self, event):
+        print(self.uiRootList.selectedIndex)
+        self.root_id = self.notion_id_list[self.uiRootList.selectedIndex]
 
-    def openFile(self, fileext, filedesc, fileparm):
-        myFilePath = ''
-        chooseFile = JFileChooser()
-        myFilter = FileNameExtensionFilter(filedesc,[fileext])
-        chooseFile.setFileFilter(myFilter)
-        ret = chooseFile.showOpenDialog(self.tab)
-        if ret == JFileChooser.APPROVE_OPTION:
-            file = chooseFile.getSelectedFile()
-            myFilePath = str(file.getCanonicalPath()).lower()
-            if not myFilePath.endswith(fileext):
-                myFilePath += '.' + fileext
-            okWrite = JOptionPane.YES_OPTION
-            if os.path.isfile(myFilePath):
-                okWrite = JOptionPane.showConfirmDialog(self.tab,'File already exists. Ok to over-write?','',JOptionPane.YES_NO_OPTION)
-                if okWrite == JOptionPane.NO_OPTION:
-                    return
-            j = True
-            while j:
-                try:
-                    f = open(myFilePath,mode=fileparm)
-                    j = False
-                except IOError:
-                    okWrite = JOptionPane.showConfirmDialog(self.tab,'File cannot be opened. Correct and retry?','',JOptionPane.YES_NO_OPTION)
-                    if okWrite == JOptionPane.NO_OPTION:
-                        return None, False
-            return f, True
+    def wraperToExportNotion(self, e):
+        tmp_token = self.uiTokenField.getText()
+        self.nw = NotionWrapper(token=tmp_token)
+        jdata = self.nw.get_pages()
+        for data in jdata:
+            print(data["parent"]["type"])
+            if data["parent"]["type"] == "page_id":
+                self.notion_id_list.append(data["id"])
+                self.notion_title_list.append(data["properties"]["title"]["title"][0]["text"]["content"])
+            else:
+                print('parent is database skiping since no title available')
+        self.uiRootList = swing.JList(self.notion_title_list, valueChanged=self.menu_click)
+        self.uiRootPagePane.setViewportView(self.uiRootList)
+
+    def exportAndSaveToNotion(self, e):
+        # [ 'method', 'status', 'path_root', 'rest of path', ... ]
+        root_saved = []
+        api_id_root = []
+        if self.nw == None or self.root_id == None:
+            return
+
+        db_id = self.nw.create_root_database_from_template(self.root_id)
+        for line in self.req_data:
+            if line[1] != '200': # ignore not 200 requests
+                continue
+            if line[2] in root_saved:
+                self.nw.create_child_page_from_template(api_id_root[root_saved.index(line[2])], '/'.join(line[2:]), line[0])
+            else:
+                page_id = self.nw.create_root_page_from_template(db_id, line[2])
+                inline_id = self.nw.create_inline_database_from_template(page_id)
+                root_saved.append(line[2])
+                api_id_root.append(inline_id)
+                self.nw.create_child_page_from_template(inline_id, '/'.join(line[2:]), line[0])
 
     def populate_array(self, status, uri, method):
         data_uri = uri.split('/')[1:]
         if isinstance(data_uri, str) or len(data_uri) == 0:
             return
+        data_uri = [sub if len(sub) else sub.replace('', '/') for sub in data_uri]
         data_uri.insert(0, status)
         data_uri.insert(0, method)
-        if len(self.csv_data) == 0:
-            self.csv_data.append(data_uri)
+        if len(self.req_data) == 0:
+            self.req_data.append(data_uri)
         else:
             i = 0
             j = 0
-            while i < len(self.csv_data) and j < len(data_uri):
+            while i < len(self.req_data) and j < len(data_uri):
                 if self.bigest_length < len(data_uri):
                     self.bigest_length = len(data_uri)
-                if data_uri in self.csv_data:
+                if data_uri in self.req_data:
                     return
-                if j == len(self.csv_data[i]):
+                if j == len(self.req_data[i]):
                     i = i + 1
                     j = 0
                     continue
-                if self.csv_data[i][j] != data_uri[j]:
+                if self.req_data[i][j] != data_uri[j]:
                     #split array and insert
-                    self.csv_data.insert(i, data_uri)
+                    self.req_data.insert(i, data_uri)
                     break
                 else:
                     # elements are equal so go back up
@@ -288,7 +368,7 @@ class BurpExtender(IBurpExtender, IHttpListener, ITab):
         if self.bigest_length > len(self.colName):
             for i in range(len(self.colName), self.bigest_length):
                 self.colName.append('path')
-        dataModel = DefaultTableModel(self.csv_data, self.colName)
+        dataModel = DefaultTableModel(self.req_data, self.colName)
         self.uiLogTable = swing.JTable(dataModel)
         self.uiLogTable.setAutoCreateRowSorter(True);
         self.uiLogPane.setViewportView(self.uiLogTable)
